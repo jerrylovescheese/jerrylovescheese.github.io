196264dc76c5149a71b3e5e958bea8ff0814a7a0
Jerry Cyan <jerrylovesapple@icloud.com>
2024-04-04 17:23:01 +0800

    Updated Apr 4 2024

---
+++ assets/js/header.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#app-element-2").click(function () {
+        $(".app-element-pop-ups-3").toggleClass("active");
+    });
+});
+$(function () {
+    $("#app-element-4").click(function () {
+        $("#app-element-pop-ups-1").toggleClass("active");
+    });
+});
+$(function () {
+    $("#app-element-5").click(function () {
+        $("#app-element-pop-ups-2").toggleClass("active");
+    });
+});
